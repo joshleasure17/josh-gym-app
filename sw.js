df51a5ba5ef2a3a4a585b796224cd00f1bbf7f5c
joshleasure17 <j925jv6khg@privaterelay.appleid.com>
2026-09-22 20:39:00 -0400

    Bump cache for mood check-in

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="josh-gym-v15";
+const CACHE="josh-gym-v16";
 const STATIC=["./manifest.webmanifest?v=3","./app-icon.svg?v=3"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
